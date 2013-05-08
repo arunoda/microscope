cd411fcb5fb0f0d7cf458f6e4e5fd400b239257d
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2013-05-08 10:53:23 +0530

    added basic template

--- client/main.js
+++
@@ -1,9 +0,0 @@
-Template.hello.greeting = function() {
-    return "Hello World to Microscope!";
-};
-
-Template.hello.events({
-    'click input': function() {
-        console.log("You clicked a button via Microscope");        
-    }
-});
