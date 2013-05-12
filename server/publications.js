a8aa4efed8f9f37b2be1bcfd5908b0ce4d76a528
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2013-05-12 16:27:53 +0530

    added publish with subscription callbacks

--- server/publications.js
+++ server/publications.js
@@ -1,5 +1,23 @@
 Meteor.publish('posts', function() {
-    return Posts.find({});
+    var sub = this;
+    var handle = Posts.find({}).observe({
+        added: function(doc) {
+            sub.added('posts', doc._id, doc);
+        },
+
+        changed: function(newDoc, oldDoc) {
+            sub.changed('posts', oldDoc._id, newDoc);
+        },
+
+        removed: function(oldDoc) {
+            sub.removed('posts', oldDoc._id);
+        }
+    });
+
+    sub.ready();
+    sub.onStop(function() {
+        handle.stop();
+    });
 });
 
 Meteor.publish('comments', function(postId) { 
